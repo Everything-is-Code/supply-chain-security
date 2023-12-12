08a491366d77ed66703d4722116d2e7a4bdfe4d2
Pablo Castelo <pablo.castelo@gmail.com>
2023-12-11 21:07:44 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,14 +3,13 @@ OC_COMMAND="oc <command>"
 # Check if the operator is installed
 oc apply -k ./bootstrap/argocd/
 OPERATOR_NAME="openshift-gitops-operator"
-OPERATOR_STATUS=$(oc get clusteroperators | grep "$OPERATOR_NAME" | awk '{print $2}')
-
+OPERATOR_STATUS=$(oc get subscriptions -n openshift-operators | grep openshift-gitops)
 # Wait until the operator is installed
-if [[ "$OPERATOR_STATUS" != "Installed" ]]; then
+if [[ ! "$OPERATOR_STATUS"]]; then
   echo "Waiting for operator '$OPERATOR_NAME' to be installed..."
-  while [[ "$OPERATOR_STATUS" != "Installed" ]]; do
+  while [[ ! "$OPERATOR_STATUS" ]]; do
     sleep 5
-    OPERATOR_STATUS=$(oc get clusteroperators | grep "$OPERATOR_NAME" | awk '{print $2}')
+    OPERATOR_STATUS=$(oc get subscriptions -n openshift-operators | grep openshift-gitops)
   done
 fi
 
